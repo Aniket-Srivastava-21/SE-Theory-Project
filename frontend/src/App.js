bce1fc05dac35cd8606d3d293d60466014dcae66
Aniket-Srivastava-21 <60710662+Aniket-Srivastava-21@users.noreply.github.com>
2021-10-29 11:50:28 +0530

    Fixed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,12 +8,38 @@ import StudentDashboard from './MyComponents/StudentDashboard';
 import MentorDashboard from './MyComponents/MentorDashboard';
 import MentorFeedback from './MyComponents/MentorFeedback';
 import CourseDashboard from './MyComponents/CourseDashboard';
+import { ExamPage } from './MyComponents/Exampage';
+import { CoursePage } from './MyComponents/CoursePage';
+import { Login } from './MyComponents/Login';
+import { Register } from './MyComponents/Register';
+import { Footer } from './MyComponents/Footer'
+import { Header } from './MyComponents/Header'
+import { MainPage } from './MyComponents/MainPage'
 
 function App() {
   return (
     <Router>
+    
+      <Header/>
+    
       <Switch>
-        
+    
+        <Route exact path="/exam">
+          <ExamPage/>
+        </Route>
+    
+        <Route exact path="/courses">
+          <CoursePage/>
+        </Route>
+    
+        <Route exact path="/login">
+          <Login/>
+        </Route>
+    
+        <Route exact path="/register">
+          <Register/>
+        </Route>
+    
         <Route exact path='/mentordashboard'>
           <MentorDashboard />
         </Route>
@@ -25,11 +51,19 @@ function App() {
         <Route exact path="/courseDashboard">
           <CourseDashboard/>
         </Route>
+    
         <Route exact path="/feedback">
           <MentorFeedback/>
         </Route>
 
+        <Route exact path="/">
+          <MainPage/>
+        </Route>
+    
       </Switch>
+    
+      <Footer/>
+    
     </Router>
 
   );
