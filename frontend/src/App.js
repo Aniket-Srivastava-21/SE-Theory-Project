70f0d3866eab6d57e0ca9c14e4f9d9ebc63719da
Himanshu8862 <81471989+Himanshu8862@users.noreply.github.com>
2021-10-29 10:57:27 +0530

    updated app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,29 @@
-import React from 'react'
-import { Footer } from './MyComponents/Footer'
-import { Header } from './MyComponents/Header'
-import { MainPage } from './MyComponents/MainPage'
+import './App.css';
 import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
+  BrowserRouter as Router,
+  Switch,
+  Route,
 } from "react-router-dom";
-import { ExamPage } from './MyComponents/Exampage';
-import { CoursePage } from './MyComponents/CoursePage';
-import { Login } from './MyComponents/Login';
-import { Register } from './MyComponents/Register';
+import StudentDashboard from './MyComponents/StudentDashboard';
+import MentorDashboard from './MyComponents/MentorDashboard';
 
-const App = () => {
+function App() {
   return (
     <Router>
-        <Header/>
-        <Switch>
-			<Route exact path="/exam">
-				<ExamPage/>
-			</Route>
-			<Route exact path="/courses">
-				<CoursePage/>
-			</Route>
-			<Route exact path="/login">
-            	<Login/>
-          	</Route>
-			<Route exact path="/register">
-            	<Register/>
-          	</Route>
-          	<Route exact path="/">
-            	<MainPage/>
-          	</Route>
-        </Switch>
-        <Footer/>
+      <Switch>
+        
+        <Route exact path='/mentordashboard'>
+          <MentorDashboard />
+        </Route>
+
+        <Route exact path='/studentdashboard'>
+          <StudentDashboard />
+        </Route>
+
+      </Switch>
     </Router>
-  )
+
+  );
 }
 
 export default App;
