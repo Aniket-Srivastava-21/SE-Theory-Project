2da2844fadd901548c76b95f3d6d87a7f895daf7
Abhinav <guptaabhinav707@gmail.com>
2021-11-05 10:34:44 +0530

    added payment module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,81 +1,97 @@
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import StudentDashboard from './MyComponents/StudentDashboard';
-import MentorDashboard from './MyComponents/MentorDashboard';
-import MentorFeedback from './MyComponents/MentorFeedback';
-import CourseDashboard from './MyComponents/CourseDashboard';
-import { ExamPage } from './MyComponents/Exampage';
-import { CoursePage } from './MyComponents/CoursePage';
-import { Login } from './MyComponents/Login';
-import { Register } from './MyComponents/Register';
-import { Footer } from './MyComponents/Footer'
-import { Header } from './MyComponents/Header'
-import { MainPage } from './MyComponents/MainPage'
-import ForgotPassword from './MyComponents/ForgotPassword';
-import MentorForm from './MyComponents/MentorForm';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import StudentDashboard from "./MyComponents/StudentDashboard";
+import MentorDashboard from "./MyComponents/MentorDashboard";
+import MentorFeedback from "./MyComponents/MentorFeedback";
+import CourseDashboard from "./MyComponents/CourseDashboard";
+import { ExamPage } from "./MyComponents/Exampage";
+import { CoursePage } from "./MyComponents/CoursePage";
+import { Login } from "./MyComponents/Login";
+import { Register } from "./MyComponents/Register";
+import { Footer } from "./MyComponents/Footer";
+import { Header } from "./MyComponents/Header";
+import { MainPage } from "./MyComponents/MainPage";
+import DebitCard from "./MyComponents/DebitCard";
+import Payment from "./MyComponents/Payment";
+import InternetBanking from "./MyComponents/InternetBanking";
+import CreditCard from "./MyComponents/CreditCard";
+import PaymentSuccess from "./MyComponents/PaymentSuccess";
+import Upi from "./MyComponents/Upi"
+
+import ForgotPassword from "./MyComponents/ForgotPassword";
+import MentorForm from "./MyComponents/MentorForm";
 
 function App() {
   return (
     <Router>
-    
-      <Header/>
-    
+      <Header />
+
       <Switch>
-    
         <Route exact path="/exam">
-          <ExamPage/>
+          <ExamPage />
         </Route>
-    
+
         <Route exact path="/courses">
-          <CoursePage/>
+          <CoursePage />
         </Route>
-    
+
         <Route exact path="/login">
-          <Login/>
+          <Login />
         </Route>
-    
+
         <Route exact path="/register">
-          <Register/>
+          <Register />
         </Route>
-    
-        <Route exact path='/mentordashboard'>
+
+        <Route exact path="/mentordashboard">
           <MentorDashboard />
         </Route>
 
-        <Route exact path='/studentdashboard'>
+        <Route exact path="/studentdashboard">
           <StudentDashboard />
         </Route>
 
         <Route exact path="/courseDashboard">
-          <CourseDashboard/>
+          <CourseDashboard />
         </Route>
-    
+
         <Route exact path="/feedback">
-          <MentorFeedback/>
+          <MentorFeedback />
         </Route>
 
         <Route exact path="/forgotpassword">
-          <ForgotPassword/>
+          <ForgotPassword />
         </Route>
 
         <Route exact path="/mentorform">
-          <MentorForm/>
+          <MentorForm />
         </Route>
 
         <Route exact path="/">
-          <MainPage/>
+          <MainPage />
         </Route>
-    
+        <Route exact path="/payment">
+          <Payment />
+        </Route>
+        <Route exact path="/debitcard">
+            <DebitCard />
+        </Route>
+        <Route exact path="/creditcard">
+          <CreditCard />
+        </Route>
+        <Route exact path="/paymentsuccess">
+          <PaymentSuccess />
+        </Route>
+        <Route exact path="/internetbanking">
+          <InternetBanking />
+        </Route>
+        <Route exact path="/upi">
+          <Upi />
+        </Route>
       </Switch>
-    
-      <Footer/>
-    
-    </Router>
 
+      <Footer />
+    </Router>
   );
 }
 
